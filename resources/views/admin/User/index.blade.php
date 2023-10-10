2c04989fb0a3b0c54a467a2e66a4d580e2780fbe
ramaf <ramaf2526@gmail.com>
2023-10-10 21:00:08 +0700

    anu

--- resources/views/admin/User/index.blade.php
+++ resources/views/admin/User/index.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.master')
 @section('title', 'Category')
 @section('content')
-
+<table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Nama</th>
+                          <th>umur</th>
+                          <th>nohp</th>
+                          <th>email</th>
+                          <th>ijazah</th>
+                          <th>foto</th>
+                          <th>lamaran</th>
+                          <th>alamat</th>
+                          <th>password</th>
+                          <th>ttl</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row"></th>
+                          
+                        </tr>
+                        <tr>
+                          <th scope="row"></th>
+                          
+                        </tr>
+                        <tr>
+                          <th scope="row"></th>
+                          
+                        </tr>
+                      </tbody>
+                    </table>
 @endsection
